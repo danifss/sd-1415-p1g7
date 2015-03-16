9399bf132f10508e858fba953a4a90f3bc04097e
daniel.silva <daniel.silva@ua.pt>
2015-03-16 17:29:06 +0000

    Adicionado os ficheiros: MemFIFO e MemObject. MonShop: FIFO de espera de clientes criada.

---
+++ SD-Projeto01/src/MonitorsProblema1/MemFIFO.java
@@ -0,0 +1,50 @@
+package MonitorsProblema1;
+
+/**
+ * Descrição geral: este tipo de dados define uma memória de tipo fifo derivada
+ * a partir de uma memória genérica.
+ */
+public class MemFIFO extends MemObject {
+
+	/**
+	 * Definição da memória de tipo fifo
+	 */
+	private int inPnt, outPnt;                   // ponteiros de inserção e de retirada de um valor
+	private boolean empty;                       // sinalização de memória vazia
+
+	/**
+	 * Construtor de variáveis
+	 */
+	public MemFIFO(int nElem) {
+		super(nElem);
+		inPnt = outPnt = 0;
+		empty = true;
+	}
+
+	/**
+	 * fifo in -- escrita de um valor
+	 */
+	@Override
+	public void write(Object val) {
+		if ((inPnt != outPnt) || empty) {
+			mem[inPnt] = val;
+			inPnt = (inPnt + 1) % mem.length;
+			empty = false;
+		}
+	}
+
+	/**
+	 * fifo out -- leitura de um valor
+	 */
+	@Override
+	public Object read() {
+		Object val = null;
+
+		if (!empty) {
+			val = mem[outPnt];
+			outPnt = (outPnt + 1) % mem.length;
+			empty = (inPnt == outPnt);
+		}
+		return val;
+	}
+}
