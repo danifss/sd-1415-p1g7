2bab3e233c294e6183bca22e9f8b1b0a9853f5fc
daniel.silva <daniel.silva@ua.pt>
2015-03-16 13:26:47 +0000

    MainProblem atualizado.

---
+++ SD-Projeto01/src/MainProblema01/Customer.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainProblema01;
+
+/**
+ *
+ * @author Daniel 51908
+ * @author Raphael 64044
+ * @version 1.0
+ */
+public class Customer {
+    public void livingNormalLife(){
+        
+    }
+    
+    public void goShopping(){
+        
+    }
+    
+    public void isDoorOpen(){
+        
+    }
+    
+    public void tryAgainLater(){
+        
+    }
+    
+    public void enterShop(){
+        
+    }
+    
+    public void perusingAround(){
+        
+    }
+    
+    public void iWantThis(){
+        
+    }
+    
+    public void exitShop(){
+        
+    }
+}
