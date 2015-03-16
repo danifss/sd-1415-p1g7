0e100424984565afbf229d014e9e738451a11618
raphael.carvalho@ua.pt <raphael.carvalho@ua.pt@fd2af15e-ec1e-48d0-b3fe-f5b3acf87e43>
2015-03-16 14:13:49 +0000

    Customer: - Construtor; - Método run() e livingNormalLife() (Incompleto);

--- SD-Projeto01/src/MainProblema01/Customer.java
+++ SD-Projeto01/src/MainProblema01/Customer.java
@@ -5,42 +5,69 @@
  */
 package MainProblema01;
 
+import MonitorsProblema1.*;
+
 /**
  *
  * @author Daniel 51908
  * @author Raphael 64044
  * @version 1.0
  */
-public class Customer {
+public class Customer extends Thread{
+    /**
+     * Customer identity
+     *
+     * @serialField customerId
+     */
+    private int customerId;
+    
+    /**
+     * Number of cycle iterations of the client life
+     * 
+     * @serialField nIter
+     */
+    private int nIter = 0;
+    
+    /**
+     * Shop
+     * 
+     * @serialField shop
+     */
+    
+    private MonShop shop;
+            
+    /**
+     * Create customer thread
+     * 
+     * @param customerId customer identification
+     * @param shop Shop
+     * @param nIter number of cycle iterations of the client life
+     */
+            
+    public Customer(int customerId, MonShop shop, int nIter){
+        this.customerId = customerId;
+        this.shop = shop;
+        if(nIter>0) this.nIter = nIter;
+    }
+    
+    /**
+     * Life cycle of the customer
+     */
+    
+    @Override
+    public void run(){
+        for(int i=0; i<nIter; i++){
+            livingNormalLife();
+        }
+    }
+        
+    /**
+     * Living normal life
+     */
     public void livingNormalLife(){
-        
+        try{
+            sleep((long) (1+40*Math.random()));
+        }catch(InterruptedException e){}
     }
     
-    public void goShopping(){
-        
-    }
-    
-    public void isDoorOpen(){
-        
-    }
-    
-    public void tryAgainLater(){
-        
-    }
-    
-    public void enterShop(){
-        
-    }
-    
-    public void perusingAround(){
-        
-    }
-    
-    public void iWantThis(){
-        
-    }
-    
-    public void exitShop(){
-        
-    }
 }
