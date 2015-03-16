e2813a8fe3539148c0a84ae87784c58d77fe80ae
daniel.silva@ua.pt <daniel.silva@ua.pt@fd2af15e-ec1e-48d0-b3fe-f5b3acf87e43>
2015-03-16 13:26:47 +0000

    MainProblem atualizado.

---
+++ SD-Projeto01/src/MainProblema01/Main.java
@@ -0,0 +1,20 @@
+package MainProblema01;
+
+/**
+ *
+ * @author Daniel 51908
+ * @author Raphael 64044
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /**
+		 * Aqui temos de lançar 3 artesãos, 1 loja, e 3 clientes
+		 */
+    }
+    
+}
