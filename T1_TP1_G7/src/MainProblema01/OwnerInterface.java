fe7fb26ecc8ba6f01db81474f65a19256d13ea94
raphael.carvalho <raphael.carvalho@ua.pt>
2015-04-04 01:28:31 +0000

    Owner & OwnerInterface: Javadoc

--- T1_TP1_G7/src/MainProblema01/OwnerInterface.java
+++ T1_TP1_G7/src/MainProblema01/OwnerInterface.java
@@ -1,18 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MainProblema01;
 
 /**
- *
- * @author Daniel
+ * @author Daniel 51908
+ * @author Raphael 64044
+ * @version 1.0
  */
 public interface OwnerInterface {
 
     /**
-     * Life cycle of the owner
+     * Life cycle of the owner.
      */
     void run();
     
