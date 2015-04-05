ae9c7c1c1aa1e4e763e5580b7a1307606a1db005
daniel.silva@ua.pt <daniel.silva@ua.pt@fd2af15e-ec1e-48d0-b3fe-f5b3acf87e43>
2015-04-05 10:45:18 +0000

    Criados os package-info do MainProblema01 e MonitorsProblema1

---
+++ T1_TP1_G7/src/MainProblema01/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * Static solution of the Aveiro Handicraft problem.
+ * This package contains the necessary threads.
+ */
+package MainProblema01;
---
+++ T1_TP1_G7/src/MonitorsProblema1/package-info.java
@@ -0,0 +1,5 @@
+/*
+ * Static solution of the Aveiro Handicraft problem.
+ * This package contains the necessary Monitors.
+ */
+package MonitorsProblema1;
